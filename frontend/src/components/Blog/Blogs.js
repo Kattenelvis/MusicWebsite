cf085080a33234ff114b756cc9c3d883fc6ff5e6
Kattenelvis <officialkattenelvis@gmail.com>
2019-09-30 22:15:32 +0200

    Added categories tab

--- frontend/src/components/Blog/Blogs.js
+++ frontend/src/components/Blog/Blogs.js
@@ -15,7 +15,26 @@ export default function Blogs() {
 
 	useEffect(() => {
 		getBlogs()
+		SetLinksToTags()
 	}, [])
+
+	//Gives function to the list underneath "Categories" in the aside
+	const SetLinksToTags = () => {
+		const tags = document.querySelector('.sidebar ul')
+		for (let i = 0; i < tags.children.length; i++) {
+			const li = tags.children[i]
+
+			li.addEventListener('click', () => {
+				for (let i = 0; i < tags.children.length; i++) {
+					tags.children[i].classList.remove('selectedTag')
+				}
+
+				li.classList.toggle('selectedTag')
+				if (i === 0) sortByTag('')
+				else sortByTag(li.innerText)
+			})
+		}
+	}
 
 	const sortByTag = tag => {
 		if (tag === '')
@@ -31,8 +50,18 @@ export default function Blogs() {
 	}
 
 	return (
-		<div>
-			{blogList}
+		<div className='Blogs'>
+			<aside className='sidebar'>
+				<h2>Categories</h2>
+				<ul>
+					<li>All</li>
+					<li>Plugins</li>
+					<li>Industry</li>
+					<li>Money</li>
+					<li>Other</li>
+				</ul>
+			</aside>
+			<div className='blogList'>{blogList}</div>
 			<div className='loading'></div>
 		</div>
 	)
