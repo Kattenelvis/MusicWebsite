80b2f94e9a2e67c8d53fa84f8a952c1b40694ac4
Kattenelvis <officialkattenelvis@gmail.com>
2019-10-01 08:46:32 +0200

    Begun working on Plugins

--- frontend/src/components/Plugins/Plugin.js
+++ frontend/src/components/Plugins/Plugin.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Plugin() {
 	return (
 		<div className='plugin'>
-			<img src={'../../img/EDMScene.jpg'} />
+			<img src={require('../../img/EDMScene.jpg')} width='250px' height='160px' />
+			<h2>Super Cool Synth With Long Name</h2>
+			<h3>51 reviews ⭐⭐⭐⭐⭐</h3>
+			<h3>$20</h3>
+			<button className='btn btn-primary'>BUY NOW</button>
 		</div>
 	)
 }
