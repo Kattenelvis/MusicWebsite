c509a52eadc922684b89d72f9bd0ed577f9f3045
Kattenelvis <officialkattenelvis@gmail.com>
2019-10-19 16:02:00 +0200

    added firebase

--- frontend/src/components/Blog/BlogLink.js
+++ frontend/src/components/Blog/BlogLink.js
@@ -13,11 +13,8 @@ export default function Blogs({ blog }) {
 					alt='...'
 				/>
 				<div className='media-body'>
-					<h5 className='mt-0'>Media heading</h5>
-					Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-					sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra
-					turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue
-					felis in faucibus.
+					<h5 className='mt-0'>{blog.title}</h5>
+					{blog.description}
 				</div>
 				</Link>
 			</div>
