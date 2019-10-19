f454076e910688c7d3ba71491d776071191366bf
Kattenelvis <officialkattenelvis@gmail.com>
2019-10-19 15:30:28 +0200

    Fixed samples

--- frontend/src/components/Samples/Sample.js
+++ frontend/src/components/Samples/Sample.js
@@ -1,19 +1,20 @@
 import React from 'react'
 
-export default function Sample({name, title, audio}) {
-	
+export default function Sample({name, title, file}) {
+	console.log(file)
 	return (
 		<div className='sample'>
 			<h3>By:{name}</h3>
 			<h2>{title}</h2>
-			<h3>Downloads:4615</h3>
 			<h2>
-				<a className='btn btn-info' href={audio} download>
+				<a className='btn btn-info' href={file} download>
 					DOWNLOAD
 				</a>
 			</h2>
 			<audio controls preload="metadata">
-				<source src={audio} type='audio/wav' />
+				<source src={file} type='audio/wav' />
+				<source src={file} type='audio/mp3' />
+				<source src={file} type='audio/ogg' />
 			</audio>
 		</div>
 	)
--- frontend/src/components/Samples/Samples.js
+++ frontend/src/components/Samples/Samples.js
@@ -4,14 +4,11 @@ import { Link } from 'react-router-dom'
 import Sample from './Sample'
 
 export default function Samples() {
+	console.log(require('../../audio/Bass.mp3'))
 	return (
 		<div className='mainItemDisplay'>
 			<Aside list={['Synth', 'Instruments', 'Vocal', 'Effect', 'Other']} />
 			<div className='Samples'>
-				<Sample name={"kattenelvis"} title={"Cool Bass"} file={require('../../audio/Bass.mp3')}/>
-				<Sample name={"kattenelvis"} title={"Cool Bass"} file={require('../../audio/Bass.mp3')}/>
-				<Sample name={"kattenelvis"} title={"Cool Bass"} file={require('../../audio/Bass.mp3')}/>
-				<Sample name={"kattenelvis"} title={"Cool Bass"} file={require('../../audio/Bass.mp3')}/>
 				<Sample name={"kattenelvis"} title={"Cool Bass"} file={require('../../audio/Bass.mp3')}/>
 			</div>
 			<Link to={"/samples/upload"}>Upload your own samples</Link>
